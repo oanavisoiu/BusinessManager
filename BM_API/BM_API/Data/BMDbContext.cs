f30f55955152044ad352b3b0928c891bbf81334f
oanavisoiu <oanavisoiu@gmail.com>
2023-10-08 15:51:02 +0300

    Base project

---
+++ BM_API/BM_API/Data/BMDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BM_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BM_API.Data
+{
+    public class BMDbContext : DbContext
+    {
+        public BMDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
