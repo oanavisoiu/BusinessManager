b5e96600d193e83b79d41c40dfc8c1866ceea66c
oanavisoiu <oanavisoiu@gmail.com>
2023-10-12 17:26:44 +0300

    Fixed git changes after updating .gitignore

---
+++ BM_API/DTOs/Account/RegisterDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BM_API.DTOs.Account
+{
+    public class RegisterDto
+    {
+        [Required]
+        [StringLength(15,MinimumLength =3,ErrorMessage ="First name must be at least {2} characters and max {1} characters.")]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(15, MinimumLength = 3, ErrorMessage = "Last name must be at least {2} characters and max {1} characters.")]
+        public string LastName { get; set; }
+        [Required]
+        [RegularExpression("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", ErrorMessage ="Invalid email adress.")]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(15, MinimumLength = 6, ErrorMessage = "Password must be at least {2} characters and max {1} characters.")]
+        public string Password { get; set; }
+
+    }
+}
---
+++ BM_API/Program.cs
@@ -0,0 +1,73 @@
+using BM_API.Data;
+using BM_API.Models;
+using BM_API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<BMDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("BMConnectionString")));
+
+builder.Services.AddScoped<JWTService>();
+
+builder.Services.AddIdentityCore<User>(options =>
+{
+    options.Password.RequiredLength = 6;
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = false;
+
+    options.SignIn.RequireConfirmedEmail = true;
+})
+    .AddRoles<IdentityRole>()
+    .AddRoleManager<RoleManager<IdentityRole>>()
+    .AddEntityFrameworkStores<BMDbContext>()
+    .AddSignInManager<SignInManager<User>>()
+    .AddUserManager<UserManager<User>>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"])),
+        ValidIssuer = builder.Configuration["JWT:Issuer"],
+        ValidateIssuer = true,
+        ValidateAudience = false,
+    }
+    );
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
